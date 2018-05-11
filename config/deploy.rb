2611e9f2c8de0775e5c26d31153847c0a9ad1dd8
Takeshi Yamanaka <t_yamanaka@wan55.co.jp>
2018-05-11 10:55:35 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,9 @@ lock "~> 3.10.2"
 #
 # あなたのGitHubユーザー名に変更してください
 #
-set :repo_url, "https://github.com/xxxx/20180430-schoo.git"
+set :repo_url, "https://github.com/takeshi-ymnk/20180430-schoo.git"
 
 #
 # あなたのWebサーバーの環境に合わせてデプロイ先ディレクトリパスを設定してください
 #
-set :deploy_to, "/var/www/schoo"
+set :deploy_to, "/home/takeshi-ymnk/www"
